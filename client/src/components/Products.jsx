22492ea1fd8c49f6090bd6f0cec47995a6f4dde1
chilunhay <72005042+chilunhay@users.noreply.github.com>
2023-06-23 17:37:55 +0700

    change api url

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -17,7 +17,9 @@ const Products = ({ cat, filters, sort }) => {
     const getProducts = async () => {
       try {
         const res = await axios.get(
-          cat ? `http://localhost:5000/api/products?category=${cat}` : 'http://localhost:5000/api/products',
+          cat
+            ? `https://chi-shop.onrender.com/api/products?category=${cat}`
+            : 'https://chi-shop.onrender.com/api/products',
         );
         setProducts(res.data);
       } catch (err) {}
